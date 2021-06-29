b5d078bfd68bb1a81607f9b8f6a6ea1fa88aa091
Amriddin <nazurdinov95@gmail.com>
2021-06-29 15:25:01 +0300

    impruve

--- app.js
+++ app.js
@@ -2,25 +2,22 @@ const arr = [1, 2, 5, 3, 4]
 
 function miniMaxSum(arr) {
     let sum = 0
-    let maxEl = 0
-    let minEl = 1
+    let max = 0
+
+    var len = arr.length, min = Infinity;
+  while (len--) {
+    if (Number(arr[len]) < min) {
+      min = Number(arr[len]);
+    }
+}
     
     for (let i=0; i < arr.length; i ++) {
-
-        if (arr[i] > maxEl)
-        {
-            maxEl = arr[i]
+        if(arr[i] > max) {
+            max = arr[i]
         }
-        
-        if (arr[i] < minEl)
-        {
-            minEl = arr[i]
-        }
-
         sum += arr[i]
     }
-  console.log(sum - minEl)
-  console.log(sum - maxEl)
+  console.log(sum - min)
+  console.log(sum - max)
 }
-
 miniMaxSum(arr)
